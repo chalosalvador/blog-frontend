f499054c7fb2447cb4fffe8911815632dfe945c1
chalosalvador <chalosalvador@gmail.com>
2021-02-04 15:20:39 -0500

    Auth API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { getAllArticles } from "@/lib/db";
+import { useAuth } from "@/lib/auth";
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Home({ articles }) {
+  const { register, login } = useAuth();
+
+  useEffect(() => {
+    const get = async () => {
+      await getAllArticles();
+    };
+    get();
+  });
+  const handleRegisterUser = () => {
+    const user = register({
+      email: "chalosalvador@gmail.com",
+      name: "Chalo",
+      editorial: "TEST",
+      short_bio: "YEAHAAA",
+      role: "ROLE_USER",
+      password: "123123",
+      password_confirmation: "123123",
+    });
+  };
+
+  const handleLogin = () => {
+    const user = login({
+      email: "chalosalvador@gmail.com",
+      password: "123123",
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,40 +44,14 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <button onClick={handleRegisterUser}>Registrar usuario</button>
+        <button onClick={handleLogin}>Iniciar sesión</button>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id}>{article.title}</li>
+          ))}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +60,26 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  const articles = await getAllArticles();
+
+  if (!articles) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      articles,
+    }, // will be passed to the page component as props
+  };
 }
