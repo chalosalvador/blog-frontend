9a621ff0f8330de13bc0b39e0477ec5404bdea4e
chalosalvador <chalosalvador@gmail.com>
2021-02-05 22:33:25 -0500

    Layout - Menu - Grid container - Loading state indicator - Cards - Responsive

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -4,20 +4,84 @@
 
 import { getAllArticles } from "@/lib/db";
 import Link from "next/link";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  title: {
+    overflow: "hidden",
+    display: "-webkit-box",
+    "-webkit-line-clamp": 1,
+    "-webkit-box-orient": "vertical",
+  },
+  body: {
+    overflow: "hidden",
+    display: "-webkit-box",
+    "-webkit-line-clamp": 3,
+    "-webkit-box-orient": "vertical",
+  },
+});
 
 const Articles = ({ articles }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id}>
-            <Link href={`/articles/${article.id}`}>
-              <a>{article.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Grid container spacing={2}>
+      {articles.map((article) => (
+        <Grid item xs={12} sm={6} md={4} key={article.id}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt={article.title}
+                height="140"
+                image={`http://localhost:8000/storage/${article.image}`}
+                title={article.title}
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="h2"
+                  className={classes.title}
+                >
+                  {article.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  className={classes.body}
+                >
+                  {article.body}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Compartir
+              </Button>
+              <Button size="small" color="primary">
+                <Link href={`/articles/${article.id}`}>
+                  <a>Ver más</a>
+                </Link>
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
